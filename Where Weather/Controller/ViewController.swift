10860ae54a2d911be78f195cb9ff950aba83d1fe
Alex Brown <alexanderdouglasbrown@gmail.com>
2018-01-04 03:03:21 -0800

    Potential 1.0

--- Where Weather/Controller/ViewController.swift
+++ Where Weather/Controller/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
     
     class CustomPointAnnotation: MKPointAnnotation {
         var imageName: String!
+        var placedTime = Date()
     }
     
     let locationManager = CLLocationManager()
@@ -29,6 +30,10 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
         locationManager.delegate = self
         weatherData.delegate = self
         mapView.delegate = self
+        
+        Timer.scheduledTimer(withTimeInterval: 55, repeats: true) { (_) in
+            self.popOldPins()
+        }
     }
     
     override func didReceiveMemoryWarning() {
@@ -37,15 +42,27 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
         // Dispose of any resources that can be recreated.
     }
     
+    func popOldPins(){       
+        mapView.annotations.forEach { (annotation) in
+            let pin = annotation as? CustomPointAnnotation
+            if let placedTime = pin?.placedTime {
+                let timeDifference = Date().timeIntervalSinceNow - placedTime.timeIntervalSinceNow
+                if timeDifference > 600 { // 10 minutes
+                    mapView.removeAnnotation(annotation)
+                }
+            }
+        }
+    }
+    
     func getLocation(){
-        locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.requestWhenInUseAuthorization()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.distanceFilter = 250
         locationManager.startUpdatingLocation()
     }
     
     @IBAction func mapLongPress(_ sender: UILongPressGestureRecognizer) {
-        if (sender.state == .began){
+        if sender.state == .began {
             let touchLocation = sender.location(in: mapView)
             let mapLocation = mapView.convert(touchLocation, toCoordinateFrom: mapView)
             
@@ -55,11 +72,17 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
         }
     }
     
+    @IBAction func btnClearMap(_ sender: Any) {
+        mapView.annotations.forEach { (_) in
+            mapView.removeAnnotations(mapView.annotations)
+        }
+    }
+    
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let currentLocation = locations[locations.count - 1]
         weatherData.getWeatherData(latitude: currentLocation.coordinate.latitude, longitude: currentLocation.coordinate.longitude, isUserLocation: true)
         
-        if (initialZoomIn){
+        if initialZoomIn {
             initialZoomIn = false
             mapView.selectAnnotation(mapView.userLocation, animated: true)
             let currentLocation = locations[0]
@@ -72,19 +95,50 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
         initialZoomIn = false
     }
     
+    func reportWeatherLookupError() {
+        DispatchQueue.main.sync {
+            let alert = UIAlertController(title: "Failed to Get the Weather", message: "Unable to look up the weather. Try checking your internet connection.", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alert, animated: true)
+        }
+    }
+    
     func weatherResults(data: WeatherDetails, isUserLocation: Bool) {
         DispatchQueue.main.sync {
             let roundedTempF: Int = Int(round(data.main.temp))
             
-            if (isUserLocation){
+            if isUserLocation {
                 mapView.userLocation.title = "\(data.weather[0].main), \(roundedTempF)°F"
-                mapView.userLocation.subtitle = "\(data.name)"
+                mapView.userLocation.subtitle = "\(data.name) "
+                
+                if data.sys.country != nil {
+                    for country in data.sys.country!.unicodeScalars {
+                        mapView.userLocation.subtitle?.unicodeScalars.append(UnicodeScalar(127397 + country.value)!)
+                    }
+                }
+                
+                if mapView.userLocation.subtitle == " " {
+                    mapView.userLocation.subtitle = ""
+                }
+                
             } else {
                 let annotationPoint = CustomPointAnnotation()
                 
                 annotationPoint.coordinate = CLLocationCoordinate2D(latitude: data.coord.lat, longitude: data.coord.lon)
                 annotationPoint.title = "\(data.weather[0].main), \(roundedTempF)°F"
-                annotationPoint.subtitle = "\(data.name)"
+                
+                annotationPoint.subtitle = "\(data.name) "
+                
+                if data.sys.country != nil {
+                    for country in data.sys.country!.unicodeScalars {
+                        annotationPoint.subtitle?.unicodeScalars.append(UnicodeScalar(127397 + country.value)!)
+                    }
+                }
+                
+                if annotationPoint.subtitle == " " {
+                    annotationPoint.subtitle = ""
+                }
+                
                 annotationPoint.imageName = "\(data.weather[0].icon).png"
                 
                 currentAnnotation = annotationPoint
@@ -92,16 +146,21 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
                 self.mapView.addAnnotation(annotationPoint)
             }
         }
+        
+        func addNameAndTitle(){
+            
+        }
     }
+
     
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
         
-        if !(annotation is CustomPointAnnotation){
+        if !(annotation is CustomPointAnnotation) {
             return nil
         }
         
         var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: "WhereWeather")
-        if (annotationView == nil){
+        if annotationView == nil {
             annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: "WhereWeather")
             annotationView!.canShowCallout = true
         } else {
--- Where Weather/Model/WeatherData.swift
+++ Where Weather/Model/WeatherData.swift
@@ -9,6 +9,7 @@ import UIKit
 
 protocol WeatherDelegate {
     func weatherResults(data : WeatherDetails, isUserLocation: Bool)
+    func reportWeatherLookupError()
 }
 
 struct WeatherDetails: Codable {
@@ -17,6 +18,7 @@ struct WeatherDetails: Codable {
     let weather: [Weather]
     let main: Main
     let coord: Coord
+    let sys: Sys
     
     struct Weather: Codable {
         let main: String
@@ -32,6 +34,10 @@ struct WeatherDetails: Codable {
         let lon: Double
         let lat: Double
     }
+    
+    struct Sys: Codable {
+        let country: String?
+    }
 }
 
 class WeatherData{
@@ -43,16 +49,24 @@ class WeatherData{
         let apiKey = valueForAPIKey(named: "OPEN_WEATHER_MAP")
         let url = URL(string: "http://api.openweathermap.org/data/2.5/weather?lat=\(latitude)&lon=\(longitude)&appid=\(apiKey)&units=imperial")!
         URLSession.shared.dataTask(with: url) { (data, response, error) in
-            if let data = data,
-                let result = try? JSONDecoder().decode(WeatherDetails.self, from: data) {
+            if error != nil {
+                self.delegate?.reportWeatherLookupError()
+                return
+            }
+            
+            if let data = data {
+                if let result = try? JSONDecoder().decode(WeatherDetails.self, from: data) {
                     self.delegate?.weatherResults(data: result, isUserLocation: isUserLocation)
+                } else {
+                    self.delegate?.reportWeatherLookupError()
+                }
             }
         }.resume()
     }
     
     //Just want to hide my keys, Xcode...
     //Code lifted straight from http://dev.iachieved.it/iachievedit/using-property-lists-for-api-keys-in-swift-applications/
-    func valueForAPIKey(named keyname:String) -> String {
+    private func valueForAPIKey(named keyname:String) -> String {
         // Credit to the original source for this technique at
         // http://blog.lazerwalker.com/blog/2014/05/14/handling-private-api-keys-in-open-source-ios-apps
         let filePath = Bundle.main.path(forResource: "ApiKeys", ofType: "plist")
